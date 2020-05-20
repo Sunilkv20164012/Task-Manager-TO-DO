963c2a028883a0a48fede40cb4c5b28dcef118fc
Sunilkv20164012 <Sunilkv20164012@gmail.com>
2020-05-21 03:51:48 +0530

    side navigation menu implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
-﻿import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from "@angular/material/tabs";
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from "@angular/forms";
 import { MatCardModule } from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import {CdkScrollableModule} from '@angular/cdk/scrolling';
+
+
+//navigation
+import {
+	IgxButtonModule,
+	IgxIconModule,
+	IgxLayoutModule,
+	IgxNavigationDrawerModule,
+	IgxRadioModule,
+	IgxRippleModule,
+	IgxSwitchModule,
+	IgxToggleModule
+ } from "igniteui-angular";
 
 
 // used to create fake backend
@@ -23,42 +37,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
-import { HomeComponent } from './home';;
+import { HomeComponent } from './home/home.component';
+;
 import { TaskCreateComponent, ExampleHeader } from './tasks/task-create/task-create.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        MatTabsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSidenavModule,
-        MatDialogModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatTabsModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatDialogModule, MatDatepickerModule,
+    MatNativeDateModule, MatButtonToggleModule, MatCardModule, MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    IgxButtonModule, IgxIconModule, IgxLayoutModule, IgxNavigationDrawerModule,
+    IgxRadioModule,	IgxRippleModule, IgxSwitchModule,	IgxToggleModule,
+    CdkScrollableModule
+  ],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    TaskCreateComponent,
+    ExampleHeader
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
-        MatButtonToggleModule,
-        MatCardModule,
-        MatIconModule,
-        FormsModule,
-        BrowserAnimationsModule    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        HomeComponent,
-        TaskCreateComponent,
-        ExampleHeader
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-        // provider used to create fake backend
-        fakeBackendProvider
-    ],
-    bootstrap: [AppComponent]
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { };
+export class AppModule {
+}
+;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, HostListener, Inject, OnInit, ViewChild, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { IgxNavigationDrawerComponent } from "igniteui-angular";
+import { DOCUMENT } from "@angular/common";
+
 import {MatDialog} from '@angular/material/dialog';
 
 
@@ -6,18 +9,101 @@ import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 import { TaskCreateComponent } from '../tasks/task-create/task-create.component';
 
-@Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: 'app-home-navdrawer',
+  templateUrl: './home.component.html',
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit{
     user: User;
+    public title = "Samples";
+    private isIE = !((window as any).ActiveXObject) && "ActiveXObject" in window;
+    private theme = "default-theme";
+    private styleElem: HTMLStyleElement;
+    private typefacesLoaded = ["Titillium Web", "Roboto"];
+    private typefaceUrl = "https://fonts.googleapis.com/css?family=";
 
-    constructor(private accountService: AccountService,public dialog: MatDialog) {
+    constructor(private accountService: AccountService, public dialog: MatDialog, @Inject(DOCUMENT) private document: Document) {
         this.user = this.accountService.userValue;
     }
+
     openDialog() {
       const dialogRef = this.dialog.open(TaskCreateComponent);
 
       dialogRef.afterClosed().subscribe(result => {
         console.log(`Dialog result: ${result}`);
       });
+    }
+
+
+    public ngOnInit() {
+        this.createThemeStyle();
+    }
+    @HostListener("window:message", ["$event"])
+    private onMessage(e: MessageEvent) {
+        if (e.origin === e.data.origin && typeof e.data.themeStyle === "string") {
+            this.styleElem.textContent = e.data.themeStyle;
+            const typeface = window.getComputedStyle(this.document.body).fontFamily.replace(/\"/g, "");
+            if (!(typeface.match(/,/g) || []).length &&
+                !this.typefacesLoaded.includes(typeface)) {
+                this.typefacesLoaded.push(typeface);
+                this.createTypefaceLink(typeface);
+            }
+        } else if (e.origin === e.data.origin && typeof e.data.theme === "string") {
+            this.document.body.classList.remove(this.theme);
+            this.document.body.classList.add(e.data.theme);
+            this.theme = e.data.theme;
+        }
+    }
+
+    private createTypefaceLink(typeface: string) {
+        const typefaceElem = this.document.createElement("link");
+        typefaceElem.rel = "stylesheet";
+        typefaceElem.id = "ignteui-theme-typeface";
+        typefaceElem.href = this.typefaceUrl + typeface.split(" ").join("+");
+        document.head.insertBefore(typefaceElem, this.document.head.lastElementChild);
+    }
+
+    private createThemeStyle() {
+        if (this.isIE) {
+            this.document.body.classList.add(this.theme);
+        } else {
+            this.styleElem = document.createElement("style");
+            this.styleElem.id = "igniteui-theme";
+            document.head.insertBefore(this.styleElem, this.document.head.lastElementChild);
+            this.document.body.classList.add("custom-body");
+        }
+    }
+
+
+
+
+
+    public navItems = [
+      { name: "account_circle", text: "Avatar" },
+      { name: "error", text: "Badge" },
+      { name: "group_work", text: "Button Group" },
+      { name: "home", text: "Card" },
+      { name: "view_carousel", text: "Carousel" },
+
+    ];
+    public selected = "Avatar";
+
+    @ViewChild(IgxNavigationDrawerComponent, { static: true })
+    public drawer: IgxNavigationDrawerComponent;
+
+    public drawerState = {
+      miniTemplate: false,
+      open: true,
+      pin: true
+    };
+
+    /** Select item and close drawer if not pinned */
+    public navigate(item) {
+      this.selected = item.text;
+      if (!this.drawer.pin) {
+        this.drawer.close();
+      }
     }
 }
